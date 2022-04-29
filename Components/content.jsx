babf15cbfa7809c7297e4547277cf8e044ec4bd5
leonel a matos <leonel.amatos@gmail.com>
2022-04-29 13:07:07 -0400

    chore: temp component to expand body

---
+++ Components/content.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Content() {
+	return (
+		<div style={{ minHeight: '100vh', maxHeight: '100vh', height: '100%', backgroundColor: 'pink' }}>content</div>
+	);
+}
