fef7dcc7fb02351dd45f553120036aefb9961157
leonel a matos <leonel.amatos@gmail.com>
2022-04-29 13:08:58 -0400

    refactor: change user login icon font

--- Components/Header/UserLogin.tsx
+++ Components/Header/UserLogin.tsx
@@ -1,11 +1,12 @@
 import { Box, Icon } from '@chakra-ui/react';
 import React from 'react';
 import { FiUser } from 'react-icons/fi';
+import { RiUser3Line } from 'react-icons/ri';
 
 const UserLoginIcon = () => {
 	return (
-		<Box sx={{_hover: { cursor: 'pointer' } }}>
-			<Icon as={FiUser} boxSize='1.8rem' mr='10px'/>
+		<Box sx={{ _hover: { cursor: 'pointer' } }}>
+			<Icon as={RiUser3Line} boxSize='1.8rem' mr='10px' />
 		</Box>
 	);
 };
